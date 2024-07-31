4750e902e99e5c7443743a2635d9052eb0eb9906
SakkyOP <saksham298@gmail.com>
2024-07-31 08:38:40 +0530

    Zoho Books banner added

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Button, Typography } from '@mui/material'
+import banner from '../assets/banner.png'
+import { ArrowRightAltOutlined, TipsAndUpdates, Close } from '@mui/icons-material'
 
 export default function Banner() {
   return (
-    <div>Banner</div>
+    <div className='flex relative justify-start items-center w-full bg-dark_raisin_black rounded-md border-international_klein_blue border'>
+        <img className='rounded-md w-1/4 h-full' src={banner} alt="" />
+        <div className="flex justify-center items-center w-3/4">
+            <div className="flex flex-col w-2/3">
+                <Typography variant='body1'><TipsAndUpdates fontSize='small' sx={{mb:0.3, mr: 1, color: "green"}} />Automate Bookkeeping for Payouts</Typography>
+                <Typography sx={{fontSize: 12, color: "#58637a"}} variant='body2'>Close your books 60% faster and with better accuracy by automating bookkeeping for you Payouts.</Typography>
+            </div>
+            <Button
+                sx={{pt:1, textTransform: "capitalize", fontSize: 12}}
+                variant='outlined'
+                endIcon={<ArrowRightAltOutlined/>}
+            >Connect Zoho Books</Button>
+        </div>
+        <Close sx={{position: "absolute", color: "#58637a", top: 5, right: 5}} fontSize='small' />
+    </div>
   )
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,6 @@
 import ActivityFeed from "../components/ActivityFeed"
 import AmountDisplay from "../components/AmountDisplay"
+import Banner from "../components/Banner"
 import Insights from "../components/Insights"
 import NotificationTray from "../components/NotificationTray"
 import OnboardingUpdates from "../components/OnboardingUpdates"
@@ -7,12 +8,13 @@ import TopBar from "../components/TopBar"
 
 export default function Main() {
   return (
-    <div className="flex flex-col items-center w-full">
+    <div className="flex flex-col items-center w-full pb-5">
       <TopBar />
       <div className="flex flex-col items-center justify-start w-4/6 h-full">
         <AmountDisplay />
         <NotificationTray />
         <OnboardingUpdates />
+        <Banner />
         <Insights />
         <ActivityFeed />
       </div>
